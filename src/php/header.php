49c65e6078dbeb1c11fa32a6fe414f71824c1783
LaurensHoogenboom <49717394+LaurensHoogenboom@users.noreply.github.com>
2020-06-04 17:41:26 +0200

    chrome nav color

--- src/php/header.php
+++ src/php/header.php
@@ -6,6 +6,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
 
+    <meta name="theme-color" content="#fafafa" />
+
     <meta name="keywords" content="<?php echo $pgKeywords ?>">
     <meta name="description" content="<?php echo $pgDesc ?>">
 
